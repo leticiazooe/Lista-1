216497e39740bc9bb71ede8c161aad39243d9ae5
Letícia Vitória <99629704+leticiazooe@users.noreply.github.com>
2022-05-03 15:56:33 -0300

    Add files via upload

---
+++ exercicio 1.cpp
@@ -0,0 +1,20 @@
+/*Faça um programa que receba quatro números inteiros, calcule e mostre a soma
+desses números.*/
+#include <stdio.h>
+int main ()
+{
+    int n1, n2, n3, n4,soma;
+    printf  ("\nDigite o valor do primeiro numero:\n"); //prinft= imprimir algo na tela
+    scanf ("%d%*c",&n1);
+    printf  ("\nDigite o valor do segundo numero:\n");
+    scanf ("%d%*c",&n2);
+    printf ("\nDigite o valor do terceiro numero:\n");
+    scanf ("%d%*c",&n3);
+    printf   ("\nDigite o valor do quarto numero:\n");
+    scanf ("%d%*c",&n4);
+    soma = n1+n2+n3+n4;
+    printf  ("\nO valor da soma e %d%", soma);
+    getchar ();
+    return 0;
+}
+
